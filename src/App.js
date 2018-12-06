dc7d1604d23664ca90a2e1285d1bea8ee4e54ad1
José Ronaldo Souza Junior <joseronaldosjr@gmail.com>
2018-12-06 20:09:49 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -122,14 +122,19 @@ class App extends Component {
             }
             })
           )
-          
-          this.applyFilter(campo)  
+
+            this.resetAlunos()
+
+          this.applyFilter('campus')
+          this.applyFilter('curso')
+          this.applyFilter('ano')
         }
       
       applyFilter = (campo) => {
         const {query, isset} = this.state.filters[campo]
-        this.resetAlunos()
+       
         if(isset){
+          
           if(campo === 'ano'){
             if(query.substr(0,2) !== '20'){
                 this.setState({
@@ -169,16 +174,19 @@ class App extends Component {
             })
             
           }else{
+            this.resetAlunos();
             this.setState({
               query : query,
-              alunosShown: this.state.alunosShown.filter( aluno => {
+              alunosShown: this.state.alunos.filter( aluno => {
                     if(aluno.nomeCompactado.toUpperCase().includes(query.toUpperCase())){
-                      return aluno
-                    }else if(aluno.nome.toUpperCase().includes(query.toUpperCase())) {
                       return aluno
                     }
                 })
           })
+
+          this.applyFilter('campus')
+          this.applyFilter('curso')
+          this.applyFilter('ano')
         }
     }
 
